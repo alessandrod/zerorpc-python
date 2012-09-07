e60776581b415a942351a35a66255100be7cb327
Alessandro Decina <alessandro.d@gmail.com>
2012-09-07 07:39:54 +0200

    setup: depend on pyzmq instead of pyzmq-static

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
             'argparse',
             'gevent',
             'msgpack-python',
-            'pyzmq-static>=2.1.7',
+            'pyzmq',
     ],
     tests_require=['nose'],
     test_suite='nose.collector',
